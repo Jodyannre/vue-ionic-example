d6f1de97db9304e540f19488f1766ab883b2ff82
Joddie <Jers.033@gmail.com>
2024-11-19 17:34:52 -0600

    add ionic code

---
+++ src/views/FolderPage.vue
@@ -0,0 +1,25 @@
+<template>
+    <ion-page>
+      <ion-header>
+        <ion-toolbar>
+          <ion-title>Hello world</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content class="ion-padding">
+        <h1>Content</h1>
+      </ion-content>
+      <ion-footer>
+        <ion-toolbar>
+            <ion-title>Footer</ion-title>
+        </ion-toolbar>
+      </ion-footer>
+    </ion-page>
+</template>
+  
+<script lang="ts" setup>
+ import { IonPage, IonHeader, IonContent, IonFooter, IonTitle, IonToolbar } from '@ionic/vue';
+</script>
+  
+<style scoped>
+</style>
+  
