02c0c72843c0ed28d9ab430abb2b6e36b1bbf98e
Jodyannre <2098807910101@ingenieria.usac.edu.gt>
2024-11-21 16:13:49 -0600

    add capacitor/camera example

---
+++ src/AppCapacitors.vue
@@ -0,0 +1,37 @@
+<template>
+    <ion-page>
+            <ion-header>
+                <ion-toolbar>
+                    <ion-title>Capacitor example</ion-title>
+                </ion-toolbar>
+            </ion-header>
+            <ion-content class="ion-padding">
+                <img :src="imageURL" />
+                <button @click="makePhoto">Make photo</button>
+            </ion-content>
+    </ion-page>
+</template>
+  
+<script lang="ts" setup>
+    import { IonHeader, IonToolbar, IonTitle, IonContent, IonPage } from '@ionic/vue';
+    import {Camera, CameraResultType} from '@capacitor/camera';
+    import {ref} from 'vue';
+
+    const imageURL = ref()
+
+    const makePhoto = async () => {
+        const image = await Camera.getPhoto({
+            quality: 80,
+            allowEditing: true,
+            resultType: CameraResultType.Uri
+        });
+        imageURL.value = image.webPath;
+        console.log(image);
+    }
+
+
+</script>
+  
+<style scoped>
+</style>
+  
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from './AppGrid.vue'
+import App from './AppCapacitors.vue'
 import router from './router';
 
 import { IonicVue } from '@ionic/vue';
